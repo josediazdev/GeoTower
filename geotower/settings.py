96ad1f728fd4871f70f37a28ddc781dfcbd61f68
jdiazdev <josediazmat10@gmail.com>
2025-03-22 06:49:32 -0400

    adding logic to reset password

--- geotower/settings.py
+++ geotower/settings.py
@@ -141,3 +141,11 @@ LOGIN_REDIRECT_URL = 'ui:dashboard'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
